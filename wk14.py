570acfd401233da66909ba757b228ef55c37a4bc
Fallon Myers <fmyers@gmail.com>
2022-09-19 06:58:28 +0000

    private

--- wk14.py
+++ wk14.py
@@ -2,7 +2,7 @@ import boto3
 aws_resource=boto3.resource("s3")
 bucket=aws_resource.Bucket("fallon-s3")
 response = bucket.create(
-    ACL='public-read',
+    ACL='private',
     )
 
 print(response)
